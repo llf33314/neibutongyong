8e481be479717553a7f26aefd3c706fdb2c50e96
lixaoqiang <gzhidao1010@gmail.com>
2017-10-17 10:07:26 +0800

    内部项目

---
+++ cli/src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import {
+  appTvShow
+} from '@/components/app/tvShow/router.js'
+
+Vue.use(Router)
+var routes = []
+// 合并多个路由的数组
+routes = routes.concat(appTvShow)
+
+
+const router = new Router({
+  // mode: 'history',
+  routes
+});
+
+/*router.beforeEach((to, from, next) => {
+  let user = window.JSON.parse(localStorage.getItem('user'))
+  if (!user && to.path !== '/login') {
+    next({path: '/login'})
+  } else {
+    next()
+  }
+
+  if (window.localStorage.getItem('tocken')) {
+    if (window.localStorage.getItem('tocken')) {
+      next();
+    }
+    else {
+      next({
+        path: '/login',
+        query: {redirect: to.fullPath}
+      })
+    }
+  }
+  else {
+    next();
+  }
+})*/
+export default router;
