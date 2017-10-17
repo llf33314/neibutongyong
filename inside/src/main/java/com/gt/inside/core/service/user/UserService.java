5a683e1f4a688148a6dfaa6878d6e4ff88700525
psr <gt123456>
2017-10-17 17:49:11 +0800

    增加菜单

--- inside/src/main/java/com/gt/inside/core/service/user/UserService.java
+++ inside/src/main/java/com/gt/inside/core/service/user/UserService.java
@@ -2,8 +2,11 @@ package com.gt.inside.core.service.user;
 
 import com.baomidou.mybatisplus.service.IService;
 import com.gt.inside.core.bean.user.req.LoginReq;
+import com.gt.inside.core.bean.user.res.MenuRes;
 import com.gt.inside.core.entity.user.User;
 import com.gt.inside.core.exception.user.UserException;
+
+import java.util.List;
 
 /**
  * <p>
@@ -21,4 +24,10 @@ public interface UserService extends IService<User> {
      * @throws UserException
      */
     void login(LoginReq loginReq) throws UserException;
+
+    /**
+     * 获取所有菜单
+     * @return
+     */
+    List<MenuRes> getAllMenu() throws UserException;
 }
--- inside/src/main/java/com/gt/inside/core/service/user/impl/UserServiceImpl.java
+++ inside/src/main/java/com/gt/inside/core/service/user/impl/UserServiceImpl.java
@@ -4,12 +4,19 @@ import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.gt.inside.common.enums.ResponseEnums;
 import com.gt.inside.core.bean.user.req.LoginReq;
+import com.gt.inside.core.bean.user.res.MenuRes;
 import com.gt.inside.core.dao.user.UserDAO;
+import com.gt.inside.core.entity.user.Menu;
 import com.gt.inside.core.entity.user.User;
 import com.gt.inside.core.exception.user.UserException;
+import com.gt.inside.core.service.user.MenuService;
 import com.gt.inside.core.service.user.UserService;
 import com.gt.inside.core.util.ObjectUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -21,6 +28,9 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserDAO, User> implements UserService {
+
+    @Autowired
+    MenuService menuService;
 
     /**
      * 用户登录
@@ -36,5 +46,25 @@ public class UserServiceImpl extends ServiceImpl<UserDAO, User> implements UserS
         if (ObjectUtil.isEmpty(user)){
             throw new UserException(ResponseEnums.LOGINNULL);
         }
+    }
+
+    /**
+     * 获取所有菜单
+     *
+     * @return
+     */
+    @Override
+    public List<MenuRes> getAllMenu() throws UserException {
+        EntityWrapper<Menu> entityWrapper = new EntityWrapper<>();
+        List<Menu> menuList = menuService.selectList(entityWrapper);
+        List<MenuRes> menuResList = new ArrayList<>();
+        for (Menu menu : menuList){
+            MenuRes menuRes = new MenuRes();
+            menuRes.setName(menu.getMenuName());
+            menuRes.setUrl(menu.getMenuUrl());
+            menuRes.setIcon(menu.getMenuIcon());
+            menuResList.add(menuRes);
+        }
+        return menuResList;
     }
 }
