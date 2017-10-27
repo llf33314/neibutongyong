6a1b4bfd5eb99d092c112dfcced2bacbfe29a4a3
yanhuiting <1227406194@qq.com>
2017-10-27 15:50:54 +0800

    10-27-1部门

---
+++ cli-pc/src/components/department/api/api.js
@@ -0,0 +1,17 @@
+import axios from './../../../http'
+
+let inside_base = window.INSIDE_BASE_URL;
+
+
+// 修改部门列表
+export const requestModifyDepart = params => { return axios.post(`${inside_base}/app/department/modify`, params).then(res => res.data) }
+
+// 删除部门
+export const requestDelDepart = params => { return axios.post(`${inside_base}/app/department/del`, params).then(res => res.data) }
+
+//分页获取部门列表
+export const requestListDepartInfo = params => { return axios.post(`${inside_base}/app/department/listByPage`, params).then(res => res.data) }
+
+//新增部门列表
+export const requestAddDepartInfo = params => { return axios.post(`${inside_base}/app/department/add`, params).then(res => res.data) }
+
