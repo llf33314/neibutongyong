6ce5ed0a81d4471953a194ddac94793af949f8d1
lixaoqiang <gzhidao1010@gmail.com>
2017-11-18 15:38:59 +0800

    修改路径

---
+++ cli-h5/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'app',
+  }
+
+</script>
+
+<style>
+  a {
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+    font-family: "Microsoft Yahei";
+    margin: 0;
+    padding: 0;
+  }
+
+</style>
