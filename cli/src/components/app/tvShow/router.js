bacbc3392be84c166663974cadc58c5f4156b97a
lixaoqiang <gzhidao1010@gmail.com>
2017-11-18 15:41:04 +0800

    删除cli

--- cli/src/components/app/tvShow/router.js
+++
@@ -1,8 +0,0 @@
-// 异步加载
-const index = () =>
-  import ('@/components/app/tvShow/vue/index')
-export const appTvShow = [{
-  path: '/app/index',
-  name: 'index',
-  component: index
-}]
--- cli/src/components/login/api/api.js
+++
@@ -1,5 +0,0 @@
-import axios from './../../../http'
-
-let base = 'http://192.168.3.98:7075'
-/* 登录请求 */
-export const requestLogin = params => { return axios.post(`${base}/app/login/login`, params).then(res => res.data) }
