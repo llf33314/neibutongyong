45ca8ec88e5b1e3465324576c5fd79a80da306ab
lixaoqiang <gzhidao1010@gmail.com>
2017-11-27 14:13:09 +0800

    delete cli-pc

--- cli-pc/src/components/home/router.js
+++
@@ -1,9 +0,0 @@
-// 异步加载
-const home = () => import('@/components/home/vue/home')
-export const homeAdminRouter = [
-    {
-        path: '/home',
-        name: 'home',
-        component: home,
-    }
-]
--- cli-pc/src/components/organize/department/router.js
+++
@@ -1,9 +0,0 @@
-// 异步加载
-const department = () => import('@/components/organize/department/vue/depart')
-export const departRouter = [
-    {
-        path: '/app/department',
-        name: 'department',
-        component: department,
-    }
-]
